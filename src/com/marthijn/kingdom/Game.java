72d164bbec6925e2ec525eaab5cd534f9239c377
marthijn kip <marthijn1994@gmail.com>
2017-03-03 22:51:55 +0100

    Added the screen class to render and clear the pixels on the screen.

--- src/com/marthijn/kingdom/Game.java
+++ src/com/marthijn/kingdom/Game.java
@@ -1,9 +1,16 @@
 package com.marthijn.kingdom;
 
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
 
 import javax.swing.JFrame;
+
+import com.marthijn.kingdom.graphics.Screen;
 
 public class Game extends Canvas implements Runnable {
 	private static final long serialVersionUID = 1L;
@@ -15,11 +22,17 @@ public class Game extends Canvas implements Runnable {
 	private Thread thread;
 	private JFrame frame;
 	private boolean running = false;
+	
+	private Screen screen;
+
+	private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+	private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
 
 	public Game() {
 		Dimension size = new Dimension(width * scale, height * scale);
 		setPreferredSize(size);
-		
+
+		screen = new Screen(width, height);
 		frame = new JFrame();
 	}
 
@@ -40,13 +53,37 @@ public class Game extends Canvas implements Runnable {
 
 	public void run() {
 		while (running) {
-			System.out.println("Running...");
+			update();
+			render();
 		}
+	}
+
+	public void update() {
+		
+	}
+
+	public void render() {
+		BufferStrategy bs = getBufferStrategy();
+		if (bs == null) {
+			createBufferStrategy(3);
+			return;
+		}
+
+		screen.clear();
+		screen.render();
+		for(int i = 0; i < pixels.length; i++) {
+			pixels[i] = screen.pixels[i];
+		}
+
+		Graphics g = bs.getDrawGraphics();
+		g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
+		g.dispose();
+		bs.show();
 	}
 
 	public static void main(String[] args) {
 		Game game = new Game();
-		
+
 		game.frame.setResizable(false);
 		game.frame.setTitle("2D Kingdom");
 		game.frame.add(game);
@@ -54,7 +91,7 @@ public class Game extends Canvas implements Runnable {
 		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		game.frame.setLocationRelativeTo(null);
 		game.frame.setVisible(true);
-		
+
 		game.start();
 	}
 
