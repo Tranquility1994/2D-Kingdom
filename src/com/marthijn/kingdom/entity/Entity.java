b722f7019ce3b700e0cb270f9ff671c4bfdd4f56
marthijn kip <marthijn1994@gmail.com>
2017-03-06 21:31:41 +0100

    changed

---
+++ src/com/marthijn/kingdom/entity/Entity.java
@@ -0,0 +1,47 @@
+package com.marthijn.kingdom.entity;
+
+import java.util.Random;
+
+import com.marthijn.kingdom.graphics.Screen;
+import com.marthijn.kingdom.level.Level;
+
+public class Entity {
+
+	private int x, y;
+	private boolean removed = false;
+	protected Level level;
+	protected final Random random = new Random();
+	
+	public void update() {
+		
+	}
+	
+	public void render(Screen screen) {
+		
+	}
+	
+	public void remove() {
+		removed = true;
+	}
+	
+	public boolean isRemoved() {
+		return this.removed;
+	}
+	
+	public int getX() {
+		return this.x;
+	}
+	
+	public int getY() {
+		return this.y;
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+
+}
