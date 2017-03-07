be21825e2b3a632bfbdde123c4d8b5aaab547672
marthijn kip <marthijn1994@gmail.com>
2017-03-07 19:40:03 +0100

    Added player facing directions plus sprites

--- src/com/marthijn/kingdom/entity/mob/Player.java
+++ src/com/marthijn/kingdom/entity/mob/Player.java
@@ -7,9 +7,11 @@ import com.marthijn.kingdom.input.Keyboard;
 public class Player extends Mob {
 
 	private Keyboard input;
+	private Sprite sprite;
 
 	public Player(Keyboard input) {
 		this.input = input;
+		sprite = Sprite.player_forward;
 	}
 
 	public Player(int x, int y, Keyboard input) {
@@ -29,12 +31,11 @@ public class Player extends Mob {
 	}
 
 	public void render(Screen screen) {
-		int xx = getX() - 16;
-		int yy = getY() - 16;
-		screen.renderPlayer(xx, yy, Sprite.player0);
-		screen.renderPlayer(xx + 16, yy, Sprite.player1);
-		screen.renderPlayer(xx, yy + 16, Sprite.player2);
-		screen.renderPlayer(xx + 16, yy + 16, Sprite.player3);
+		if (dir == 0) sprite = Sprite.player_forward;
+		if (dir == 1) sprite = Sprite.player_right;
+		if (dir == 2) sprite = Sprite.player_backward;
+		if (dir == 3) sprite = Sprite.player_left;
+		screen.renderPlayer(getX() - 16, getY() - 16, sprite);
 	}
 
 }
